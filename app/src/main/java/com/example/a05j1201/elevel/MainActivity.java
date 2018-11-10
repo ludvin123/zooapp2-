af5cb5879ef20adb2f65a7536e8c840b1d3a77a7
Alumno13 J901 <a13j901@campustls.local>
2018-11-10 13:11:54 -0500

    first commit

---
+++ app/src/main/java/com/example/a05j1201/elevel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a05j1201.elevel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tinder);
+    }
+}
---
+++ app/src/main/java/com/example/a05j1201/elevel/inicioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a05j1201.elevel
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class inicioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
